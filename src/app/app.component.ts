133d89cd37151d7c4e7242168ad25659d88d339f
Yauheni Hermanovich <german.yauhen@gmail.com>
2021-07-15 18:46:13 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shopping-list-course-app';
+  appTitle = 'Eugene Shopping List Course App';
 }
