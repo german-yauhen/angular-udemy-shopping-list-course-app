68e34bc88f61d1a3d301a03aaaa82130838aa045
Yauheni Hermanovich <german.yauhen@gmail.com>
2021-07-20 15:38:00 +0300

    Recipe list elements - initial code

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Pineapple cake', 'Summer garden fruits in a sweet glazed cream', 'https://images.freeimages.com/images/large-previews/b89/summer-garden-fruits-in-a-sweet-glazed-cream-pie-dessert-1632381.jpg'),
+    new Recipe('Apple pie', 'Apple pie with almonds', 'https://images.freeimages.com/images/large-previews/815/apple-pie-with-almonds-1318484.jpg')
+  ];
 
   constructor() { }
 
