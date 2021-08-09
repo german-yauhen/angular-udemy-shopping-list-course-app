5bf07a3ac727f7ca32bef0f01d8c263ec9b4aa9c
Yauheni Hermanovich <german.yauhen@gmail.com>
2021-08-09 21:54:02 +0300

    Added form array controls for ingredients

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../service/recipe.service';
@@ -30,24 +30,46 @@ export class RecipeEditComponent implements OnInit {
     );
   }
 
+  onSubimt(): void {
+    console.log(this.recipeForm);
+  }
+
   private initForm(): void {
     let recipeName: string = '';
     let recipeImagePath: string = '';
     let recipeDescription: string = '';
+
+    let recipeIngredients: FormArray = new FormArray([]);
 
     if (this.editMode) {
       const recipe: Recipe = this.recipeService.getRecipe(this.recipeId);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+
+      if (recipe['ingredients']) {
+        for (const ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'ingredientName': new FormControl(ingredient.name),
+              'ingredientAmount': new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       'recipeName': new FormControl(recipeName),
       // 'recipeAmount': new FormControl(),
       'recipeImage': new FormControl(recipeImagePath),
-      'recipeDescription': new FormControl(recipeDescription)
+      'recipeDescription': new FormControl(recipeDescription),
+      'recipeIngredients': recipeIngredients
     });
+  }
+
+  getIngredientsControls() {
+    return (this.recipeForm.get('recipeIngredients') as FormArray).controls;
   }
 
 }
