e88e256158c34ec3c5f7a568cb9622f77ebb7529
Yauheni Hermanovich <german.yauhen@gmail.com>
2021-09-03 21:25:20 +0300

    Splitting modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthComponent } from "./auth/auth.component";
-import { AuthGuard } from "./auth/auth.guard";
 import { NotFoundComponent } from "./not-found/not-found.component";
-import { RecipeContentComponent } from "./recipes/recipe-content/recipe-content.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { RecipesResolverService } from "./recipes/service/recipes-resolver.service";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  {
-    path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard],
-    children: [
-      { path: '', component: RecipeContentComponent },
-      { path: 'new', component: RecipeEditComponent },
-      { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
-      { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] }
-    ]
-  },
   { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'auth', component: AuthComponent },
-  { path: 'not-found', component: NotFoundComponent, data: {errorMessage: '404: The requested page is not found!'} },
-  { path: '**', redirectTo: '/not-found' }
+  { path: 'auth', component: AuthComponent }
+
+  // Below doesn't work!!!
+  // ,
+  // { path: 'not-found', component: NotFoundComponent, data: {errorMessage: '404: The requested page is not found!'} },
+  // { path: '**', redirectTo: '/not-found' }
 ]
 
 @NgModule({
---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "../auth/auth.guard";
+import { RecipeContentComponent } from "./recipe-content/recipe-content.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipesComponent } from "./recipes.component";
+import { RecipesResolverService } from "./service/recipes-resolver.service";
+
+const routes: Routes = [
+  {
+    path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard],
+    children: [
+      { path: '', component: RecipeContentComponent },
+      { path: 'new', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
+      { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] }
+    ]
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class RecipesRoutingModule {
+
+}
