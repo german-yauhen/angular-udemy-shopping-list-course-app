52de3c35f8494a4d91d1042f8b014000be6802e4
Yauheni Hermanovich <german.yauhen@gmail.com>
2021-11-07 19:16:05 +0300

    Resolved an issue with error message

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -27,7 +27,7 @@ export class AuthEffects {
     ofType(AuthActions.LOGIN_START),
     switchMap((authData: AuthActions.LoginStart) => {
       return this.authService.sendLoginRequest(authData.payload)
-        .pipe(map(this.handleAuthFunc), catchError(handleErrorFunc))
+        .pipe(map(this.handleAuthFunc), catchError(this.handleError))
     })
   );
 
@@ -36,7 +36,7 @@ export class AuthEffects {
     ofType(AuthActions.SIGNUP_START),
     switchMap((authData: AuthActions.SignUpStart) => {
       return this.authService.sendSignUpRequest(authData.payload)
-        .pipe(map(this.handleAuthFunc), catchError(handleErrorFunc))
+        .pipe(map(this.handleAuthFunc), catchError(this.handleError))
     })
   );
 
@@ -58,21 +58,24 @@ export class AuthEffects {
   @Effect({ dispatch: true })
   autoLogin = this.actions.pipe(
     ofType(AuthActions.AUTO_LOGIN),
-    map(() => {
-      const userData: string = localStorage.getItem('userData');
-      if (!userData) {
-        return new AuthActions.NoAutoLogin();
-      }
-      const userDataObj: { email: string, id: string, _token: string, _tokenExpirationDate: string } = JSON.parse(userData);
-      const loadedUser: User = new User(userDataObj.email, userDataObj.id, userDataObj._token, new Date(userDataObj._tokenExpirationDate));
-      if (loadedUser.token) {
-        return new AuthActions.Login(
-          { email: loadedUser.email, userId: loadedUser.id, token: loadedUser.token, expirationDate: new Date(userDataObj._tokenExpirationDate) }
-        )
-      }
-      return new AuthActions.NoAutoLogin();
-    })
+    map(() => this.handleAutoLogin())
   );
+
+  private handleAutoLogin(): AuthActions.Login | AuthActions.NoAutoLogin {
+    const userData: string = localStorage.getItem('userData');
+    if (!userData) {
+      return new AuthActions.NoAutoLogin();
+    }
+    const userDataObj: { email: string, id: string, _token: string, _tokenExpirationDate: string } = JSON.parse(userData);
+    const expDate: Date = new Date(userDataObj._tokenExpirationDate);
+    const loadedUser: User = new User(userDataObj.email, userDataObj.id, userDataObj._token, expDate);
+    if (loadedUser.token) {
+      return new AuthActions.Login(
+        { email: loadedUser.email, userId: loadedUser.id, token: loadedUser.token, expirationDate: expDate }
+      )
+    }
+    return new AuthActions.NoAutoLogin();
+  }
 
   private handleAuthFunc(rsData: AuthResponse): AuthActions.Login {
     const expirationDate: Date = new Date(new Date().getTime() + (Number.parseInt(rsData.expiresIn) * 1000));
@@ -83,42 +86,33 @@ export class AuthEffects {
     );
   };
 
-  // private doAutoLogin(): void {
-  //   const userData: string = localStorage.getItem('userData');
-  //   if (!userData) {
-  //     return;
-  //   }
-  //   const userDataObj: {
-  //     email: string,
-  //     id: string,
-  //     _token: string,
-  //     _tokenExpirationDate: string
-  //   } = JSON.parse(userData);
-  //   const loadedUser: User = new User(userDataObj.email, userDataObj.id, userDataObj._token, new Date(userDataObj._tokenExpirationDate));
-  //   if (loadedUser.token) {
-  //     this.appStore.dispatch(new AuthActions.Login(
-  //       { email: loadedUser.email, userId: loadedUser.id, token: loadedUser.token, expirationDate: new Date(userDataObj._tokenExpirationDate) }
-  //     ));
-  //     const expirationDuration: number = new Date(userDataObj._tokenExpirationDate).getTime() - new Date().getTime();
-  //     this.autoLogout(expirationDuration);
-  //   }
-  // }
+  private handleError(errorResponse: HttpErrorResponse): Observable<AuthActions.AuthFail> {
+    let errorMessage: string = 'An unknown error occurred!';
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return of(new AuthActions.AuthFail(errorMessage));
+    }
+    switch (errorResponse.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'The email address is already in use by another account';
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Password sign-in is disabled for this project';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later';
+        break;
+
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'There is no user associated with provided email address. The user may have been deleted';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password is invalid or the user does not have a password';
+        break;
+      case 'USER_DISABLED':
+        errorMessage = 'The user account has been disabled by an administrator';
+        break;
+    }
+    return of(new AuthActions.AuthFail(errorMessage));
+  };
 
 }
-
-const handleErrorFunc = function (errorResponse: HttpErrorResponse): Observable<AuthActions.AuthFail> {
-  let errorMessage: string = 'An unknown error occurred!';
-  if (!errorResponse.error || !errorResponse.error.error) {
-    return of(new AuthActions.AuthFail(errorMessage));
-  }
-  switch (errorResponse.error.error.message) {
-    case 'EMAIL_EXISTS': errorMessage = 'The email address is already in use by another account';
-    case 'OPERATION_NOT_ALLOWED': errorMessage = 'Password sign-in is disabled for this project';
-    case 'TOO_MANY_ATTEMPTS_TRY_LATER': errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later';
-
-    case 'EMAIL_NOT_FOUND': errorMessage = 'There is no user associated with provided email address. The user may have been deleted';
-    case 'INVALID_PASSWORD': errorMessage = 'The password is invalid or the user does not have a password';
-    case 'USER_DISABLED': errorMessage = 'The user account has been disabled by an administrator';
-  }
-  return of(new AuthActions.AuthFail(errorMessage));
-};
